e4f46c3bd94ab0299363a7a260a0577480d14c7b
Kanzaki <119926639+KANZAKIRU@users.noreply.github.com>
2025-09-16 17:45:50 +0800

    Update LogicwithJava3.java

--- src/pack/LogicwithJava3.java
+++ src/pack/LogicwithJava3.java
@@ -2,7 +2,7 @@ package pack;
 
 import java.util.Scanner;
 
-public class LogicwithJava3_Nacaya {
+public class LogicwithJava3 {
 
 	public static void main(String[] args) {
 		 Scanner sc = new Scanner(System.in);
@@ -21,4 +21,5 @@ public class LogicwithJava3_Nacaya {
 
 	        sc.close();
 	    }
+
 	}
